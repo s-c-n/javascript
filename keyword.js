26111271b853f39bf82639d1e323f8a3f4f7d1e1
s-c-n <samuel.nwafor975@gmail.com>
2024-07-24 04:42:45 -0700

    Add files via upload

---
+++ keyword.js
@@ -0,0 +1,27 @@
+// var x;
+// var y;
+
+// x = 5;
+// y = 6;
+// let z = x + y
+// document.write(z)
+
+// document.getElementById('demo').innerHTML = "hello"
+ 
+let grade = prompt("Enter your score")
+
+if(grade > 95){
+    document.write(" A ")
+}
+else if(grade >= 80){
+    document.write(" B ")
+}
+else if(grade >= 70){
+    document.write(" C ")
+}
+else if(grade >= 50){
+    document.write(" D ")
+}
+else{
+    document.write(" F ")
+} 
